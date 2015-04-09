01b276f94bf72d7e595327f3fe661c0d52ef6b32
brandonsouba <brandonsouba@me.com>
2015-04-09 16:43:28 -0700

    Quick math fix.
    Updated math issue.

---
+++  README.md
@@ -0,0 +1,14 @@
+Brandon Souba
+brandon@dropbox.com
+brandonsouba@me.com
+
+How many hours did it take to complete?
+3 Hours
+
+Which required and optional stories have you completed?
+Tip Calculator 
+
+![](http://i.imgur.com/SBWqG7G.gifjorda)
+
+This is a simple tip calculator for the codepath for designers class. The color of your total amount changes based on your tip amount (you better tip well slackers!). 
+
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -5,14 +5,26 @@
 //  Created by Brandon Souba on 4/8/15.
 //  Copyright (c) 2015 Brandon Souba. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var fdsfasdfsd: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var background: UIView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        totalLabel.text = "0.00"
+        billField.becomeFirstResponder()
+        self.background.backgroundColor = UIColor.grayColor()
+        self.totalLabel.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +33,71 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        if billField.text != nil {
+            UIView.animateWithDuration(0.4, animations: {
+                self.totalLabel.alpha = 1
+ 
+            })
+        }
+        
+        if billField.text == "" || billField.text == nil {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.grayColor()
+                self.totalLabel.alpha = 0
+            })
+        }
+        
+        if tipField.text == "" || billField.text == nil {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.grayColor()
+        })
+    }
+
+
+        
+
+    
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tipAmount = NSString(string: tipField.text).doubleValue
+        var tipDiscount = tipAmount * 0.01
+        var tip = tipDiscount * billAmount
+        var total = billAmount + tip
+
+        totalLabel.text = "$\(total)"
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        if tipDiscount == 0  {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.grayColor()
+            })
+        }
+        
+        if tipAmount > 0  {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.redColor()
+            })
+        }
+        
+        if tipAmount >= 15 {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.orangeColor()
+            })
+        }
+        
+        if tipAmount >= 20 {
+            UIView.animateWithDuration(0.4, animations: {
+                self.background.backgroundColor = UIColor.greenColor()
+            })
+        }
+
+
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
